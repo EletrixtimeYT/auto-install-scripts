7db49e08125d694582fba1acc80a5a8aaf2b269e
EletrixTime <71174682+EletrixtimeYT@users.noreply.github.com>
2023-09-23 14:15:07 +0200

    Update install.sh

--- linux/code/nodejs/install.sh
+++ linux/code/nodejs/install.sh
@@ -6,12 +6,15 @@ echo  GITHUB : https://github.com/EletrixtimeYT/auto-install-scripts
 echo -----------------------------
 sleep 2000
 echo Installing...
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install nodejs
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install node
-nvm install --lts
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 nvm install 8.10.0
+NODE_MAJOR=20
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 echo Finish ! Thank you for using AutoInstallScripts !
 echo Github : https://github.com/EletrixtimeYT/auto-install-scripts
